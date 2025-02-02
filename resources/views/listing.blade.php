fe756d60b63124cad8ef8ec491937f7580d4d47c
rahim8050 <rahimranxx8050@gmail.com>
2025-02-02 18:09:58 +0000

    down

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -11,12 +11,12 @@
                     >
                         <img
                             class="w-48 mr-6 mb-6"
-                            src="images/acme.png"
+                            src="{{asset ('images/no-image.png')}}"
                             alt=""
                         />
 
-                        <h3 class="text-2xl mb-2">Senior Laravel Developer</h3>
-                        <div class="text-xl font-bold mb-4">Acme Corp</div>
+                        <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
+                        <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                         <ul class="flex">
                             <li
                                 class="bg-black text-white rounded-xl px-3 py-1 mr-2"
@@ -40,7 +40,7 @@
                             </li>
                         </ul>
                         <div class="text-lg my-4">
-                            <i class="fa-solid fa-location-dot"></i> Daytona, FL
+                            <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
                         </div>
                         <div class="border border-gray-200 w-full mb-6"></div>
                         <div>
