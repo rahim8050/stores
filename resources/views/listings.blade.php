7e283dee25e191e9afb65d860dbd68980828d002
rahim8050 <rahimranxx8050@gmail.com>
2025-02-02 17:40:08 +0000

    ups

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,7 @@
 @extends('layout')
 @section('content')
+@include('partials.hero')
+@include('partials.search')
 <div
                 class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
             >
