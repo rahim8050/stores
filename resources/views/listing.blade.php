e3e41db992d7923726d9b1cf9b2ecae56c43ca6d
rahim8050 <rahimranxx8050@gmail.com>
2025-02-02 17:22:32 +0000

    ins

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,8 +1,8 @@
 @extends('layout')
 @section('content')
-<h3 class="text-2xl">
+{{-- <h3 class="text-2xl">
   <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
-</h3>
+</h3> --}}
     <p>{{ $listing->description }}</p>
   @endsection
     
