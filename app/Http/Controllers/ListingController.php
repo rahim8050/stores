1faba8260314a785c36984ce1cc32adbc732cb80
rahim8050 <rahimranxx8050@gmail.com>
2025-02-04 16:41:43 +0000

    working

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,7 +11,7 @@ class ListingController extends Controller
     {
         return view  ('listings.index', [
  
-            'listings' => Listing::latest()->filter(request(['tag']))->get()
+            'listings' => Listing::latest()->filter(request(['tag','search']))->get()
             ]);
     }
     public function show(Listing $listing)
