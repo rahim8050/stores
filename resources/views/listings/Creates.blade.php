ecac65984982158136370890454e285ab9e9b90b
rahim8050 <rahimranxx8050@gmail.com>
2025-02-08 17:51:04 +0000

    pagination

--- resources/views/listings/Creates.blade.php
+++ resources/views/listings/Creates.blade.php
@@ -20,7 +20,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="company"
+                    name="company" value="{{old('company')}}"
                 />
                 @error('company')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -36,6 +36,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="title"
                     placeholder="Example: Senior Laravel Developer"
+                    value="{{old('title')}}"
                 />
                 @error('title')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -53,6 +54,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="location"
                     placeholder="Example: Remote, Boston MA, etc"
+                    value="{{old('location')}}"
                 />
                 @error('location')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -67,6 +69,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="email"
+                    value="{{old('email')}}"
                 />
                 @error('email')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -84,6 +87,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="website"
+                    value="{{old('website')}}"
                 />
                 @error('website')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -99,6 +103,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="tags"
                     placeholder="Example: Laravel, Backend, Postgres, etc"
+                    value="{{old('tags')}}"
                 />
                 @error('tags')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -113,6 +118,7 @@
                     type="file"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="logo"
+                    value="{{old('logo')}}"
                 />
                 @error('logo')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -131,6 +137,7 @@
                     name="description"
                     rows="10"
                     placeholder="Include tasks, requirements, salary, etc"
+                    {{old('description')}}
                 ></textarea>
 
         @error('description')
